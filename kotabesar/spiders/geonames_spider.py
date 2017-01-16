2ae0847541df2a503ed52eb6c9ff8a37caef9c50
pwcahyo <pwcahyo@gmail.com>
2017-01-16 13:48:43 +0700

    fix format

--- kotabesar/spiders/geonames_spider.py
+++ kotabesar/spiders/geonames_spider.py
@@ -18,22 +18,22 @@ class GeonamesSpider(Spider):
             yield Request("http://www.geonames.org/"+next_link[0], self.parse)
         itemselector = Selector(response).xpath('//div[@id="search"]/table/tr')
         for sel in itemselector:
-			item = KotabesarItem()
-			item['urutan'] = ''.join(sel.xpath('td[1]/small/text()').extract())
-			item['kota'] = ''.join(sel.xpath('td[2]/a/text()').extract())
-			item['alias'] = ''.join(sel.xpath('td[2]/small/text()').extract())
-			item['bagian'] = ''.join(sel.xpath('td[3]/text()').extract()).encode('utf8')
-			strBagian = str(item['bagian'])
-			if strBagian:
-				item['bagian'] = strBagian.replace(', ','')
-			item['feature'] = ''.join(map(unicode.strip, sel.xpath('td[4]/text()').extract()))
-			item['populasi'] = ''.join(sel.xpath('td[4]/small/text()').extract())
-			strPopulasi = str(item['populasi'])
-			if strPopulasi:
-				item['populasi'] = (strPopulasi.replace('population ','')).replace(',','')
-			item['latitude'] = ''.join(sel.xpath('td[5]/text()').extract())
-			item['longitude'] = ''.join(sel.xpath('td[6]/text()').extract())
-			yield item
+		item = KotabesarItem()
+		item['urutan'] = ''.join(sel.xpath('td[1]/small/text()').extract())
+		item['kota'] = ''.join(sel.xpath('td[2]/a/text()').extract())
+		item['alias'] = ''.join(sel.xpath('td[2]/small/text()').extract())
+		item['bagian'] = ''.join(sel.xpath('td[3]/text()').extract()).encode('utf8')
+		strBagian = str(item['bagian'])
+		if strBagian:
+			item['bagian'] = strBagian.replace(', ','')
+		item['feature'] = ''.join(map(unicode.strip, sel.xpath('td[4]/text()').extract()))
+		item['populasi'] = ''.join(sel.xpath('td[4]/small/text()').extract())
+		strPopulasi = str(item['populasi'])
+		if strPopulasi:
+			item['populasi'] = (strPopulasi.replace('population ','')).replace(',','')
+		item['latitude'] = ''.join(sel.xpath('td[5]/text()').extract())
+		item['longitude'] = ''.join(sel.xpath('td[6]/text()').extract())
+		yield item
         
 
 
